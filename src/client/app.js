66f5aba23ec0065aca4547efe64bf8aebee82050
Jenn Wilde <jennowilde@gmail.com>
2018-02-06 16:56:03 -0800

    Fixes error with bundling css files

--- src/client/app.js
+++ src/client/app.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 
+import './style.css';
+
 export default class App extends Component {
     render() {
         return <div>Welcome to React Boilerplate App</div>
