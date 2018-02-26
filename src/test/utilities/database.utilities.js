e2bfb21f77a10b4ee1e24edf6fc5547a9c0e040b
Jenn Wilde <jennowilde@gmail.com>
2018-02-26 15:22:25 -0800

    Simplifies truncateDatabase function

--- src/test/utilities/database.utilities.js
+++ src/test/utilities/database.utilities.js
@@ -1,4 +1,4 @@
-import database from '../model/database';
+import database from '../../server/model/database';
 
 export function getTables() {
 	return database.query(`
@@ -10,7 +10,9 @@ export function getTables() {
 export function truncateDatabase() {
 	return getTables()
 		.then((tables) => {
-			return database.multi(tables.map(`TRUNCATE ${table.table_name} RESTART IDENTITY CASCADE;`).join(''));
+			return database.multi(tables.map((table) => {
+				return `TRUNCATE ${table.table_name} RESTART IDENTITY CASCADE;`;
+			}).join(''));
 		})
 		.catch((error) => {
 			console.error(error);
